692dfa82b2b1bae2d6db91b58e15541c6a30296a
Emre Balcı <122218470+emrbalc1@users.noreply.github.com>
2023-06-25 17:06:20 +0300

    Butonlu form örneği yapıldı

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+function App() {
+  const [bfiyat, bfiyatGuncelle] = useState("");
+  const [adet, adetGuncelle] = useState("");
+  const [kdv, kdvGuncelle] = useState("");
+  const [sonuc, sonucGuncelle] = useState(0);
+
+  function formGonderim(olay) {
+    olay.preventDefault();
+
+    let tutar = parseInt(bfiyat) * parseInt(adet)
+    tutar = tutar + (tutar/100) * parseInt(kdv)
+    
+    sonucGuncelle(tutar)
+  }
+
+  return (
+    <>
+      <form method="post" onSubmit={formGonderim}>
+        <input
+          type="text"
+          placeholder="Birim Fiyat"
+          onChange={(olay) => bfiyatGuncelle(olay.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Adet"
+          onChange={(olay) => adetGuncelle(olay.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="KDV % ör: 18"
+          onChange={(olay) => kdvGuncelle(olay.target.value)}
+        />
+        <button type="submit">Hesapla</button>
+      </form>
+
+      <p>{sonuc}</p>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <p>Merhaba!</p>
+  <>
+    <App />
+  </>
 );
